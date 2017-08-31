37eba9b0a6d697e9d636e2d8f477c06f5e2920d0
Jason Perkins <starkos@industriousone.com>
2017-08-31 15:59:36 -0400

    Pass direct fetch of simple value unit test

--- query.lua
+++ query.lua
@@ -2,17 +2,20 @@
 -- query/query.lua
 --
 -- Author Jason Perkins
--- Copyright (c) 2016-2017 Jason Perkins and the Premake project
+-- Copyright (c) 2017 Jason Perkins and the Premake project
 ---
 
-local m = {}
+	local m = {}
+
+	local p = premake
 
 
 ---
 -- Set up ":" style calling.
 ---
 
-	local metatable = {
+	local metatable =
+	{
 		__index = function(self, key)
 			return m[key]
 		end
@@ -26,11 +29,81 @@ local m = {}
 ---
 
 	function m.new(source)
-		local self = {
+		local self =
+		{
+			_configSet = source
 		}
+
 		setmetatable(self, metatable)
+
 		return self
 	end
 
 
-return m
+
+---
+-- Fetch a value, applying the previously specified filtering criteria.
+--
+-- *Values returned from this function should be considered immutable!* I
+-- don't have a way to enforce that (yet), so you'll just have to be on
+-- your best behavior. If you change a value returned from this method,
+-- you may be changing it for all future calls as well. Make copies before
+-- making changes, and be on your best behavior!
+---
+
+	function m.fetch(self, key)
+		local value
+
+		local field = p.field.get(key)
+
+		if p.field.merges(field) then
+			value = m.fetchCollection(self, field)
+		else
+			value = m.fetchPrimitive(self, field)
+		end
+
+		return value
+	end
+
+
+
+---
+-- Fetch a collection (e.g. list, set) value, applying the previously
+-- specified filtering criteria.
+---
+
+	function m.fetchCollection(self, field)
+		return nil
+	end
+
+
+
+---
+-- Fetch a primitive (e.g. string, number) value, applying the previously
+-- specified filtering criteria.
+---
+
+	function m.fetchPrimitive(self, field)
+		local blocks = self._configSet.blocks
+		local key = field.name
+
+		-- Walk the list of blocks backwards; exit on first value found
+		local n = #blocks
+		for i = n, 1, -1 do
+			local block = blocks[i]
+			local value = block[key]
+
+			if value ~= nil then
+				return value
+			end
+		end
+
+		return nil
+	end
+
+
+---
+-- End of module
+--
+
+	return m
